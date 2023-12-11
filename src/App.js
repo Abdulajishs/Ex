587fd0060ff0cd9626e56aec100cc98f78da3bae
Abdulajishs <abdulajeeshsb@gmail.com>
2023-12-11 15:54:47 +0530

    Passing Data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenses = [{
+    title: "toilet paper",
+    amount: 94.12,
+    date: new Date(2023, 2, 27),
+    LocationOfExpenditure: "Supermarket"
+  }, {
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2023, 3, 28),
+    LocationOfExpenditure: "Insurance company"
+  }, {
+    title: "New Desk(wooden)",
+    amount: 450,
+    date: new Date(2023, 5, 12),
+    LocationOfExpenditure: "Furniture shop"
+  }
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+        LocationOfExpenditure={expenses[0].LocationOfExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+        LocationOfExpenditure={expenses[1].LocationOfExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+        LocationOfExpenditure={expenses[2].LocationOfExpenditure}
+      ></ExpenseItem>
+
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
-function ExpenseItem() {
+import "./ExpenseItem.css"
+
+function ExpenseItem(props) {
     return (
-    <div>
-        <h2>Expense item!</h2>
-    <ul>
-        <li>Food Rs 10</li>
-        <li>Petrol Rs 100</li>
-        <li>Movies Rs 200</li>
-    </ul>
-    </div>
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <h2>{props.LocationOfExpenditure}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
     )
 }
 
