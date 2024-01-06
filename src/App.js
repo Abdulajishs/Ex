89cf8d702a33e5dcac5ec82fa23c99be6be1a2d3
Abdulajishs <abdulajeeshsb@gmail.com>
2024-01-06 09:26:58 +0530

    Rendering Expenses with performance optimisations

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewExpense from "./components/NEW EXPENSES/NewExpenses";
 import Expenses from "./components/EXPENSES/Expenses";
 
-const App = () => {
+const DUMMY_EXPENSES = [{
+  id: "e1",
+  title: "toilet paper",
+  amount: 94.12,
+  date: new Date(2023, 2, 27),
+  LocationOfExpenditure: "Supermarket"
+}, {
+  id: "e2",
+  title: "Car Insurance",
+  amount: 294.67,
+  date: new Date(2023, 3, 28),
+  LocationOfExpenditure: "Insurance company"
+}, {
+  id: "e3",
+  title: "New Desk(wooden)",
+  amount: 450,
+  date: new Date(2023, 5, 12),
+  LocationOfExpenditure: "Furniture shop"
+}
+];
 
-  const expenses = [{
-    id: "e1",
-    title: "toilet paper",
-    amount: 94.12,
-    date: new Date(2023, 2, 27),
-    LocationOfExpenditure: "Supermarket"
-  }, {
-    id: "e2",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2023, 3, 28),
-    LocationOfExpenditure: "Insurance company"
-  }, {
-    id: "e3",
-    title: "New Desk(wooden)",
-    amount: 450,
-    date: new Date(2023, 5, 12),
-    LocationOfExpenditure: "Furniture shop"
-  }
-  ]
-  const addExpenseHandler = expense => {
-    console.log("In App.js");
-    console.log(expense);
-  }
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses)=>{
+      return [expense, ...prevExpenses]
+    });
+  };
+
   return (
     <div>
-      <NewExpense onAddExpense = {addExpenseHandler}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/EXPENSES/ExpenseItem.js
+++ src/components/EXPENSES/ExpenseItem.js
@@ -1,35 +1,34 @@
 import React,{ useState } from "react";
 
-import ExpenseDetails from "./ExpenseDetails";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css"
 
 const ExpenseItem = (props) => {
-    console.log("Expense item evaluated by react");
+    // console.log("Expense item evaluated by react");
 
-    const [title,setTitle] = useState(props.title)
+    // const [title,setTitle] = useState(props.title)
 
-    const clickHandler = () => {
-        setTitle("Updated!!!")
-        console.log(title);
-    }
-    const [amount,setAmount] = useState(props.amount)
+    // const clickHandler = () => {
+    //     setTitle("Updated!!!")
+    //     console.log(title);
+    // }
+    // const [amount,setAmount] = useState(props.amount)
 
-    const clickExpenseHandler = () => {
-        setAmount(100)
-        console.log(amount);
-    }
+    // const clickExpenseHandler = () => {
+    //     setAmount(100)
+    //     console.log(amount);
+    // }
     return (
         <Card  className="expense-item">
             <ExpenseDate date={props.date} />
-            <ExpenseDetails
-                title={title}
-                amount={amount}
-                location={props.location}
-            />
-            <button onClick={clickHandler}>Change Title</button>
-            <button onClick={clickExpenseHandler}>Change Expense</button>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <h2>{props.location}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+            {/* <button onClick={clickHandler}>Change Title</button>
+            <button onClick={clickExpenseHandler}>Change Expense</button> */}
         </Card>
     )
 }
