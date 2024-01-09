b79153c0a8e24faa85785d9616b380debaaaf173
Abdulajishs <abdulajeeshsb@gmail.com>
2024-01-09 23:39:27 +0530

    Conditional Rendering

--- src/components/EXPENSES/ExpenseItem.js
+++ src/components/EXPENSES/ExpenseItem.js
@@ -1,35 +1,21 @@
-import React,{ useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css"
 
 const ExpenseItem = (props) => {
-    // console.log("Expense item evaluated by react");
-
-    // const [title,setTitle] = useState(props.title)
-
-    // const clickHandler = () => {
-    //     setTitle("Updated!!!")
-    //     console.log(title);
-    // }
-    // const [amount,setAmount] = useState(props.amount)
-
-    // const clickExpenseHandler = () => {
-    //     setAmount(100)
-    //     console.log(amount);
-    // }
     return (
-        <Card  className="expense-item">
-            <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <h2>{props.location}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            {/* <button onClick={clickHandler}>Change Title</button>
-            <button onClick={clickExpenseHandler}>Change Expense</button> */}
-        </Card>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date} />
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <h2>{props.location}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>
+            </Card>
+        </li> 
     )
 }
 
--- src/components/EXPENSES/Expenses.js
+++ src/components/EXPENSES/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from "./ExpensesFilter"
 import Card from '../UI/Card';
+import ExpensesList from "./ExpensesList";
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -14,20 +14,12 @@ const Expenses = (props) => {
 
   const filteredExpense = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear
-  })
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {filteredExpense.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          location={expense.location}
-        />
-      ))}
-
+      <ExpensesList items={filteredExpense}/>
     </Card>
   );
 }
