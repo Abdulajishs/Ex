ae01a1e250418ea26bcb28010ddaf67d4eff6e95
Abdulajishs <abdulajeeshsb@gmail.com>
2024-01-10 14:35:59 +0530

    addind extra feature using conditional  rendering

--- src/components/NEW EXPENSES/NewExpenses.js
+++ src/components/NEW EXPENSES/NewExpenses.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpenses.css";
 
 const NewExpenses = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
         // console.log(expenseData);
-        props.onAddExpense(expenseData)     
+        props.onAddExpense(expenseData)
+
+        setIsEditing(false);
     }
 
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    const stopEditing = () => {
+        setIsEditing(false)
+    }
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && <ExpenseForm
+                onSaveExpenseData={saveExpenseDataHandler}
+                onCancel={stopEditing} />}
         </div>
     )
 }
